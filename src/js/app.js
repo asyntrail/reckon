3c0665ec3595378158a126eabd46939d9cd4c308
asyntrail <asyntrail@gmail.com>
2021-02-09 08:09:38 +0200

    add additional functionality

--- src/js/app.js
+++ src/js/app.js
@@ -31,11 +31,12 @@ const owss = ( Math.ceil(ows * 100) / 100 );
 document.getElementById("ow-sum").innerHTML = owss;
 
 const h = +document.getElementById('heat').value;
+
 document.getElementById("heats").innerHTML = h;
 
 const ct1 = +document.getElementById('cont1').value;
 const ct2 = +document.getElementById('cont2').value;
-const cts = ct1 + ct2;
+const cts = ( Math.ceil((ct1 + ct2) * 100) / 100 );
 document.getElementById("conts").innerHTML = cts;
 
 const e = +document.getElementById('elec').value;
@@ -50,10 +51,32 @@ document.getElementById("exports").innerHTML = ex ;
 const ph = +document.getElementById('phone').value;
 document.getElementById("phones").innerHTML = ph ;
 
+const ed = +document.getElementById('edi').value;
+document.getElementById("edis").innerHTML = ed ;
 
-const tot = (cwss*100+hwss*100+owss*100+h*100+cts*100+e*100+k*100+ex*100+ph*100)/100;
+
+const tot = (((cwss*100+hwss*100+owss*100+e*100)-ed*100)+h*100+cts*100+k*100+ex*100+ph*100)/100;
 
 document.getElementById("total").innerHTML = tot;
+
+const ps =((cwss*100+hwss*100+owss*100+e*100)-ed*100)/100;
+document.getElementById("pers_sum").innerHTML = ps ;
+const psm = +document.getElementById('pers_m').value;
+const psd = ( Math.ceil((ps/psm) * 100) / 100 );
+document.getElementById("pers_dev").innerHTML = psd ;
+
+const gs = (h*100+cts*100+k*100+ex*100+ph*100)/100;
+document.getElementById("gen_sum").innerHTML = gs ;
+const gsm = +document.getElementById('gen_m').value;
+const gsd = ( Math.ceil((gs/gsm) * 100) / 100 );
+document.getElementById("gen_dev").innerHTML = gsd ;
+
+const per1 = +document.getElementById('pers1').value;
+const pys1 = ( Math.ceil((gsd + (psd*per1)) * 100) / 100 );
+const per2 = +document.getElementById('pers2').value;
+const pys2 = ( Math.ceil((gsd + (psd*per2)) * 100) / 100 );
+document.getElementById("pay_sum1").innerHTML = pys1 ;
+document.getElementById("pay_sum2").innerHTML = pys2 ;
 
 }
 
